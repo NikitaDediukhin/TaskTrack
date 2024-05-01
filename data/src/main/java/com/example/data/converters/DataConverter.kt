1892842f39780293bdbdc8ac3ac773aaa31adc84
Nikita <dedukhin.nickita@ya.ru>
2024-05-01 17:50:38 +0300

    Added data converter, changed TaskModel's properties

---
+++ data/src/main/java/com/example/data/converters/DataConverter.kt
@@ -0,0 +1,15 @@
+package com.example.data.converters
+
+import androidx.room.TypeConverter
+import java.util.Date
+
+class DataConverter {
+    @TypeConverter
+    fun timeStampToDate(value: Long?): Date? {
+        return if (value == null) null else Date(value)
+    }
+    @TypeConverter
+    fun dateToTimeStamp(date: Date?): Long? {
+        return date?.time
+    }
+}
--- domain/src/main/java/com/example/domain/models/TaskModel.kt
+++ domain/src/main/java/com/example/domain/models/TaskModel.kt
@@ -1,12 +1,12 @@
 package com.example.domain.models
 
-import java.time.LocalDate
+import java.util.Date
 
 data class TaskModel(
     private val id: Int,
     private val title: String,
     private val description: String,
-    private val creationDate: LocalDate,
-    private val dueDate: LocalDate,
+    private val creationDate: Date,
+    private val dueDate: Date,
     private val competitionStatus: Boolean
 )
