d27afd6f1cbd4ea0b32102956df94febfbcd6ee1
Nikita <dedukhin.nickita@ya.ru>
2024-05-12 21:38:25 +0300

    Added FragmentUtils.kt & dialog integration to TasksFragment

--- app/src/main/java/com/example/tasktrack/fragment/TasksFragment.kt
+++ app/src/main/java/com/example/tasktrack/fragment/TasksFragment.kt
@@ -5,7 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.tasktrack.R
 import com.example.tasktrack.databinding.TasksFragmentBinding
+import com.example.tasktrack.utils.DialogManager
+import java.util.Date
 
 class TasksFragment: Fragment() {
 
@@ -16,13 +19,27 @@ class TasksFragment: Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val binding = TasksFragmentBinding.inflate(inflater, container, false)
+        binding = TasksFragmentBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val btnAddTask: View = binding.root.findViewById(R.id.btnAddTask)
+
+        btnAddTask.setOnClickListener{
+            showAddTaskDialog()
+        }
     }
 
+    private fun showAddTaskDialog() {
+        context?.let { context ->
+            DialogManager.addTaskDialog(context, object : DialogManager.Listener {
+                override fun onClick(title: String, description: String, changeDate: Date, dueDate: Date) {
+                }
+            })
+        }
+    }
 
 }
---
+++ app/src/main/java/com/example/tasktrack/utils/FragmentUtils.kt
@@ -0,0 +1,64 @@
+package com.example.tasktrack.utils
+
+import android.annotation.SuppressLint
+import android.app.DatePickerDialog
+import android.app.Dialog
+import android.content.Context
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.LinearLayout
+import com.example.tasktrack.R
+import com.google.android.material.textfield.TextInputEditText
+import java.util.Calendar
+import java.util.Date
+
+object DialogManager {
+    @SuppressLint("InflateParams")
+    fun addTaskDialog(context: Context, listener: Listener){
+        val dialog = Dialog(context)
+
+        dialog.window?.run {
+            setContentView(R.layout.add_task_dialog)
+            setLayout(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT)
+        }
+        dialog.setCancelable(false)
+        val btnAddTask: Button = dialog.findViewById(R.id.btnAddTask)
+        val btnChangeDate: Button = dialog.findViewById(R.id.btnTaskDueDate)
+        val ivCancel: ImageView = dialog.findViewById(R.id.ivCancel)
+        val etTaskTitle: TextInputEditText = dialog.findViewById(R.id.editTaskTitle)
+        val etTaskDescription: TextInputEditText = dialog.findViewById(R.id.editTaskDescription)
+
+        lateinit var dueDate: Date
+        val calendar = Calendar.getInstance()
+
+        ivCancel.setOnClickListener {
+            dialog.dismiss()
+        }
+        btnChangeDate.setOnClickListener {
+            val datePickerDialog = DatePickerDialog(context, {DatePicker, year: Int, monthOfYear: Int, dayOfMonth: Int ->
+                calendar.set(year, monthOfYear, dayOfMonth)
+
+                dueDate = calendar.time
+            },
+            calendar.get(Calendar.YEAR),
+            calendar.get(Calendar.MONTH),
+            calendar.get(Calendar.DAY_OF_MONTH)
+            )
+            datePickerDialog.show()
+        }
+        btnAddTask.setOnClickListener {
+            listener.onClick(
+                title = etTaskTitle.text.toString(),
+                description = etTaskDescription.text.toString(),
+                changeDate = Date(),
+                dueDate = dueDate
+            )
+        }
+
+        dialog.show()
+    }
+
+    interface Listener{
+        fun onClick(title: String, description: String, changeDate: Date, dueDate: Date)
+    }
+}
