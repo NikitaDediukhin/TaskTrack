5fad37c3b2dba39a7072226eb11ec96220fe4d13
Nikita <dedukhin.nickita@ya.ru>
2024-05-12 21:16:23 +0300

    Added fragment in activity

--- app/src/main/java/com/example/tasktrack/activity/MainActivity.kt
+++ app/src/main/java/com/example/tasktrack/activity/MainActivity.kt
@@ -3,10 +3,15 @@ package com.example.tasktrack.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.tasktrack.R
+import com.example.tasktrack.fragment.TasksFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragmentContainer, TasksFragment())
+            .commit()
     }
 }
